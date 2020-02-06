9bdf20fdb842f302931b3ffd5e119a6d95f4144f
markoeror <erormarko.84@gmail.com>
2020-02-06 20:59:06 +0100

    login-signup

---
+++ src/app/_helpers/index.ts
@@ -0,0 +1,4 @@
+﻿export * from "./auth.guard";
+export * from "./error.interceptor";
+export * from "./authorisation";
+export * from "./jwt.interceptor";
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+﻿import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+
+// used to create fake backend
+import { authorisationProvider } from "./_helpers";
+
+import { AppComponent } from "./app.component";
+import { appRoutingModule } from "./app.routing";
+
+import { JwtInterceptor, ErrorInterceptor } from "./_helpers";
+import { HomeComponent } from "./home";
+import { LoginComponent } from "./login";
+import { SigninComponent } from "./signin/signin.component";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    appRoutingModule
+  ],
+  declarations: [AppComponent, HomeComponent, LoginComponent, SigninComponent],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    authorisationProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
