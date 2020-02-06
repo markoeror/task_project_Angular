cbadcc99648a11bbb0122f0fdc4691ad9a977708
Danilo Stevanovic <danilo.stevanovic@zentity.com>
2020-02-07 00:47:03 +0100

    #theme

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { tasks } from '../../mock/mock.db';
 
 @Component({
-    selector: 'app-test',
-    templateUrl: 'test.component.html'
+  selector: 'app-test',
+  templateUrl: 'test.component.html'
 })
-
 export class TestComponent implements OnInit {
-    constructor() { }
-
-    ngOnInit() { }
+  constructor() {}
+  public tasks = tasks;
+  ngOnInit() {}
 }
